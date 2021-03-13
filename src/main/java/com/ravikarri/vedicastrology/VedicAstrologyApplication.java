9378e8a7605c743c2f51d66c9542003457abdccd
Ravi Shanker Reddy Karri <me@ravikarri.in>
2021-03-13 23:18:58 +0530

    Initial Commit

---
+++ src/main/java/com/ravikarri/vedicastrology/VedicAstrologyApplication.java
@@ -0,0 +1,13 @@
+package com.ravikarri.vedicastrology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedicAstrologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VedicAstrologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravikarri/vedicastrology/VedicAstrologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravikarri.vedicastrology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VedicAstrologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
